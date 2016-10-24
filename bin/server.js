6a7e9af862b7dd52ab7076551d9f78d1ed52cfc5
AR <a>
2016-10-23 22:42:27 -0700

    everything must go

--- bin/server.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config')
-const server = require('../server/main')
-const debug = require('debug')('app:bin:server')
-const port = config.server_port
-const host = config.server_host
-
-server.listen(port, host)
-debug(`Server is now running at http://${host}:${port}.`)
--- src/routes/Home/containers/HomeContainer.js
+++
@@ -1,4 +0,0 @@
-import HomeView from '../components/HomeView'
-import { connect } from 'react-redux'
-
-export default connect(state => ({ queue: state.getIn(['queue','queue']), username: state.getIn(['queue','userId']), admin: state.getIn(['queue','admin']) }))(HomeView)
